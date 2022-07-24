81804aed62f49fb6ab377544c4ba7a73e8915734
jaortizv <56170742+jaortizv@users.noreply.github.com>
2022-07-24 16:18:32 -0500

    Add files via upload

---
+++ secondCsound.c
@@ -0,0 +1,135 @@
+/*
+This program uses Beep(x,y) function
+x: frequency
+y: milliseconds
+*/
+
+
+int main()
+{
+	int x,y;
+	
+	x = 130; /*close to a C*/
+	y = 2000;
+	Beep(x,y);
+	
+	x = 196; /*close to a G*/
+	y = 2000;
+	Beep(x,y);
+	
+	x = 261;
+	y = 1900;
+	Beep(x,y);
+	
+	x = 329;
+	y = 400;
+	Beep(x,y);
+	
+	x = 311;
+	y = 3000;
+	Beep(x,y);
+	
+	sleep(1);
+	
+		
+	x = 130;
+	y = 2000;
+	Beep(x,y);
+	
+	x = 196;
+	y = 2000;
+	Beep(x,y);
+	
+	x = 261;
+	y = 1900;
+	Beep(x,y);
+	
+	x = 311;
+	y = 400;
+	Beep(x,y);
+	
+	x = 329;
+	y = 2000;
+	Beep(x,y);
+	
+	x = 523;
+	y = 400;
+	Beep(x,y);
+	
+	x = 698;
+	y = 2000;
+	Beep(x,y);
+	
+	x = 880;
+	y = 400;
+	Beep(x,y);
+	
+	x = 987;
+	y = 400;
+	Beep(x,y);
+	
+	x = 1046;
+	y = 800;
+	Beep(x,y);
+	
+	x = 1174;
+	y = 1500;
+	Beep(x,y);
+	
+	x = 1318;
+	y = 400;
+	Beep(x,y);
+	
+	x = 1396;
+	y = 400;
+	Beep(x,y);
+	
+	x = 1567;
+	y = 2000;
+	Beep(x,y);
+	
+
+	x = 659;
+	y = 400;
+	Beep(x,y);
+	
+	x = 523;
+	y = 400;
+	Beep(x,y);
+	
+	x = 392;
+	y = 400;
+	Beep(x,y);
+	
+	x = 329;
+	y = 400;
+	Beep(x,y);
+	
+	x = 1318;
+	y = 400;
+	Beep(x,y);
+	
+	x = 1396;
+	y = 400;
+	Beep(x,y);
+	
+	x = 1567;
+	y = 2000;
+	Beep(x,y);
+	
+	x = 1760;
+	y = 2000;
+	Beep(x,y);
+	
+	x = 1975;
+	y = 2000;
+	Beep(x,y);
+	
+	x = 1046;
+	y = 4000;
+	Beep(x,y);
+	
+	return 0;
+}
+
+
